87aab427b74076336dcb029e5ae74e48b41d567f
Christophe Costa <costachristophe065@gmail.com>
2022-06-14 10:42:16 -0300

    completando a parte do retorno no console

--- palindromo.js
+++ palindromo.js
@@ -14,10 +14,12 @@ function verificaPalindromo2 (string){
     if(!string) return "string inesistente";
 
     for(let i = 0; i < string.lenght / 2; i++) {
-        if(string[i] !== string.lenght -) {
-            if(string [i] !== string.lenght - 1 - i){
-                
-            }
+        if(string[i] !== string[string.lenght - 1 -i]) {
+         return false;
         }
     }
+
+    return true;
 }
+
+console.log(verificaPalindromo2("aabbaa"))
